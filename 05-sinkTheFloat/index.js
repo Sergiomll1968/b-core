86cfcf424e88c1c3f78165601c81ea37cc5ad246
sergiomll1968 <sergiomaciaslluch@gmail.com>
2023-04-16 13:06:35 +0200

    act hundir la flota 20230416 v1

--- 05-sinkTheFloat/index.js
+++ 05-sinkTheFloat/index.js
@@ -4,7 +4,6 @@ const root = document.getElementById('root');
 
 const maxRows = 10;
 const maxColumns = 10;
-let getOponentName = false;
 let oponentName = 'Pon aquí tu nombre';
 let playing = false;
 let numberOfShots = 0;
@@ -68,53 +67,94 @@ const gameOponentBoard = [
   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 ];
 
-// Creación de titulo del juego y asignación de estilos definidos en CSS
+// Crear y mostrar titulo del juego y asignación de estilos definidos en CSS
 
 const titleInfo = document.createElement('div');
 titleInfo.setAttribute('class', 'titleInfo');
 titleInfo.textContent = 'Juguemos a "hundir la flota"';
 root.appendChild(titleInfo);
 
-// Mostrar pagina principal con instrucciones y obtención del nombre del oponente
+// Crear y mostrar pagina principal con instrucciones y obtención del nombre del oponente
 
 const mainPageContainer = document.createElement('div');
 mainPageContainer.setAttribute('class', 'mainPageContainer');
 root.appendChild(mainPageContainer);
 
 const gameRulesTitle = document.createElement('div');
-gameRulesTitle.setAttribute('class','gameRulesTitle');
+gameRulesTitle.setAttribute('class', 'gameRulesTitle');
 gameRulesTitle.textContent = 'Instrucciones del juego';
 mainPageContainer.appendChild(gameRulesTitle);
 
-const gameRulesText = document.createElement('div');
-gameRulesText.setAttribute('class','gameRulesText');
-gameRulesText.textContent = 'Introduce tu nombre para jugar';
-mainPageContainer.appendChild(gameRulesText);
+const playerNameTitle = document.createElement('div');
+playerNameTitle.setAttribute('class', 'playerNameTitle');
+playerNameTitle.textContent = 'Introduce tu nombre para jugar';
+mainPageContainer.appendChild(playerNameTitle);
 
-const gameRulesContainer = document.createElement('div');
-gameRulesContainer.setAttribute('class', 'gameRulesContainer');
-gameRulesContainer.textContent = 'Lorem ipsum dolor sit amet consectetur';
-mainPageContainer.appendChild(gameRulesContainer);
+const gameRulesText1 = document.createElement('div');
+gameRulesText1.setAttribute('class', 'gameRulesText');
+gameRulesText1.textContent = '1.- El juego consiste en hundir la flota del contrincante. Para ello, debe colocar su propia flota de forma estratégica y encontrar y hundir con los disparos la flota contraria.';
+mainPageContainer.appendChild(gameRulesText1);
 
-const oponentNameContainer = document.createElement('div');
-oponentNameContainer.setAttribute('class', 'oponentNameContainer');
-oponentNameContainer.textContent = 'Aqui va el cuadro de texto y un boton';
-mainPageContainer.appendChild(oponentNameContainer);
+const playerNameInput = document.createElement('div');
+playerNameInput.setAttribute('class', 'playerNameInput');
 
+const nameInput = document.createElement('input');
+nameInput.setAttribute('type', 'text');
+nameInput.setAttribute('name', 'name');
+nameInput.addEventListener('keypress', checkTypedText);
+nameInput.setAttribute('placeholder', oponentName);
 
+const buttonValidate = document.createElement('input');
+buttonValidate.setAttribute('type', 'submit');
+buttonValidate.setAttribute('value', 'Validar');
+buttonValidate.addEventListener('click', validateInputName);
 
-do {
+mainPageContainer.appendChild(playerNameInput);
+playerNameInput.appendChild(nameInput);
+playerNameInput.appendChild(buttonValidate);
 
-  // Obtener nombre del oponente
-  oponentName = 'Sergio';
+const gameRulesText2 = document.createElement('div');
+gameRulesText2.setAttribute('class', 'gameRulesText');
+gameRulesText2.textContent = '2.- Cada uno de los jugadores dispone de dos cuadrículas que ocultará al otro jugador: en una debe colocar sus barcos y en la otra irá anotando los resultados de los disparos que realiza en cada turno. En la página siguiente dispone de plantillas de las cuadrículas.';
+mainPageContainer.appendChild(gameRulesText2);
 
-} while (oponentName === 'Pon aquí tu nombre');
+const gameRulesText3 = document.createElement('div');
+gameRulesText3.setAttribute('class', 'gameRulesText');
+gameRulesText3.textContent = '3.- Cada jugador debe colocar en uno de los cuadros los siguientes barcos en posición horizontal o vertical: 1 barco que ocupa 5 cuadrados, otro de 4, otro de 3, otro de 2 y otro de 1';
+mainPageContainer.appendChild(gameRulesText3);
+
+const gameRulesText4 = document.createElement('div');
+gameRulesText4.setAttribute('class', 'gameRulesText');
+gameRulesText4.textContent = '4.- Los barcos se tienen que colocar respetando una franja de cuadros en blanco alrededor. Sí pueden colocarse junto a los bordes de la cuadrícula, pero sin llegar a pegarse un barco con otro.';
+mainPageContainer.appendChild(gameRulesText4);
+
+const gameRulesText5 = document.createElement('div');
+gameRulesText5.setAttribute('class', 'gameRulesText');
+gameRulesText5.textContent = '5.- Cada jugador dispone de un turno de disparo que se irá alternando. Para hacerlo dirá las coordenadas. Por ejemplo “B3”, significa que su disparo corresponde a la casilla que se encuentra en esa coordenada.';
+mainPageContainer.appendChild(gameRulesText5);
+
+const gameRulesText6 = document.createElement('div');
+gameRulesText6.setAttribute('class', 'gameRulesText');
+gameRulesText6.textContent = '6.- Al disparar, el otro jugador comprobará el resultado en su tablero: si en la casilla se encuentra parte de un barco el jugador tiene derecho a un nuevo disparo en el mismo turno.';
+mainPageContainer.appendChild(gameRulesText6);
+
+const gameRulesText7 = document.createElement('div');
+gameRulesText7.setAttribute('class', 'gameRulesText');
+gameRulesText7.textContent = '7.- El jugador que dispara anota en su cuadro de disparos los resultados. Si los tiros son “agua”, marcará con un punto la cuadrícula; si los disparos son “tocado” o “hundido”, los puede marcar con una cruz. De esta forma el jugador puede saber las cuadrículas que quedan en blanco y en las que ya ha disparado.';
+mainPageContainer.appendChild(gameRulesText7);
+
+const gameRulesText8 = document.createElement('div');
+gameRulesText8.setAttribute('class', 'gameRulesText');
+gameRulesText8.textContent = '8.- Finalmente, gana el jugador que antes consigue hundir la flota del otro.';
+mainPageContainer.appendChild(gameRulesText8);
+
+nameInput.focus();
 
 // Creación de subtitulos del juego y asignación de estilos definidos en CSS
 
 const subtitlesContainer = document.createElement('div');
 subtitlesContainer.setAttribute('class', 'subtitlesContainer');
-root.appendChild(subtitlesContainer);
+// root.appendChild(subtitlesContainer);
 
 const subtitleInfo1 = document.createElement('div');
 subtitleInfo1.setAttribute('class', 'subtitleInfo');
@@ -123,14 +163,14 @@ subtitlesContainer.appendChild(subtitleInfo1);
 
 const subtitleInfo2 = document.createElement('div');
 subtitleInfo2.setAttribute('class', 'subtitleInfo');
-subtitleInfo2.textContent = 'Oponente';
+// subtitleInfo2.textContent = oponentName;
 subtitlesContainer.appendChild(subtitleInfo2);
 
 // Creación de los dos tableros en el DOM y asignación de estilos definidos en CSS
 
 const boardsContainer = document.createElement('div');
 boardsContainer.setAttribute('class', 'boardsContainer');
-root.appendChild(boardsContainer);
+// root.appendChild(boardsContainer);
 
 const gameSpace1 = document.createElement('div');
 gameSpace1.setAttribute('class', 'gameSpace');
@@ -214,7 +254,7 @@ for (let i = 0; i < 100; i++) {
 
 const gameInfo = document.createElement('div');
 gameInfo.setAttribute('class', 'gameInfo');
-root.appendChild(gameInfo);
+// root.appendChild(gameInfo);
 
 const startGameButton = document.createElement('button');
 startGameButton.setAttribute('class', 'button');
@@ -224,7 +264,7 @@ gameInfo.appendChild(startGameButton);
 
 const turnInfo = document.createElement('p');
 turnInfo.setAttribute('class', 'turnInfo');
-turnInfo.textContent = `Turno de: ${isComputerTurn ? 'Ordenador' : 'Oponente'}`;
+// turnInfo.textContent = `Turno de: ${isComputerTurn ? 'Ordenador' : oponentName}`;
 gameInfo.appendChild(turnInfo);
 
 const shotsInfo = document.createElement('p');
@@ -232,7 +272,92 @@ shotsInfo.setAttribute('class', 'shotsInfo');
 shotsInfo.textContent = `Número de jugadas: ${numberOfShots}`;
 gameInfo.appendChild(shotsInfo);
 
-// Funciones del juego
+// Crear y mostrar pagina de resultados al terminar el juego
+
+// const mainPageContainer = document.createElement('div');
+// mainPageContainer.setAttribute('class', 'mainPageContainer');
+// root.appendChild(mainPageContainer);
+
+// const gameRulesTitle = document.createElement('div');
+// gameRulesTitle.setAttribute('class', 'gameRulesTitle');
+// gameRulesTitle.textContent = 'Instrucciones del juego';
+// mainPageContainer.appendChild(gameRulesTitle);
+
+// const playerNameTitle = document.createElement('div');
+// playerNameTitle.setAttribute('class', 'playerNameTitle');
+// playerNameTitle.textContent = 'Introduce tu nombre para jugar';
+// mainPageContainer.appendChild(playerNameTitle);
+
+// const gameRulesText1 = document.createElement('div');
+// gameRulesText1.setAttribute('class', 'gameRulesText');
+// gameRulesText1.textContent = '1.- El juego consiste en hundir la flota del contrincante. Para ello, debe colocar su propia flota de forma estratégica y encontrar y hundir con los disparos la flota contraria.';
+// mainPageContainer.appendChild(gameRulesText1);
+
+// const playerNameInput = document.createElement('div');
+// playerNameInput.setAttribute('class', 'playerNameInput');
+
+// const nameInput = document.createElement('input');
+// nameInput.setAttribute('type', 'text');
+// nameInput.setAttribute('name', 'name');
+// nameInput.addEventListener('keypress', checkTypedText);
+// nameInput.setAttribute('placeholder', oponentName);
+
+// const buttonValidate = document.createElement('input');
+// buttonValidate.setAttribute('type', 'submit');
+// buttonValidate.setAttribute('value', 'Validar');
+// buttonValidate.addEventListener('click', validateInputName);
+
+// mainPageContainer.appendChild(playerNameInput);
+// playerNameInput.appendChild(nameInput);
+// playerNameInput.appendChild(buttonValidate);
+
+// const gameRulesText2 = document.createElement('div');
+// gameRulesText2.setAttribute('class', 'gameRulesText');
+// gameRulesText2.textContent = '2.- Cada uno de los jugadores dispone de dos cuadrículas que ocultará al otro jugador: en una debe colocar sus barcos y en la otra irá anotando los resultados de los disparos que realiza en cada turno. En la página siguiente dispone de plantillas de las cuadrículas.';
+// mainPageContainer.appendChild(gameRulesText2);
+
+// const gameRulesText3 = document.createElement('div');
+// gameRulesText3.setAttribute('class', 'gameRulesText');
+// gameRulesText3.textContent = '3.- Cada jugador debe colocar en uno de los cuadros los siguientes barcos en posición horizontal o vertical: 1 barco que ocupa 5 cuadrados, otro de 4, otro de 3, otro de 2 y otro de 1';
+// mainPageContainer.appendChild(gameRulesText3);
+
+// nameInput.focus();
+
+// Función que valida que se introduzca un nombre de oponente y si es asi, cambia a la pantalla de juego
+
+function validateName() {
+
+  if (nameInput.value !== '') {
+
+    oponentName = nameInput.value;
+
+    root.replaceChild(subtitlesContainer, mainPageContainer);
+    root.appendChild(boardsContainer);
+    root.appendChild(gameInfo);
+
+    // nameInput.setAttribute('placeholder', oponentName);
+    subtitleInfo2.textContent = oponentName;
+    turnInfo.textContent = `Turno de: ${isComputerTurn ? 'Ordenador' : oponentName}`;
+
+  }
+
+}
+
+function validateInputName() {
+  validateName();
+}
+
+function checkTypedText(event) {
+
+  if (event.key === 'Enter') {
+
+    validateName();
+
+  }
+
+}
+
+// Resto de funciones del juego
 
 function coordinatesToId(row, column) {
 
@@ -342,22 +467,39 @@ function checkRight(row, column) {
 
 function displayShot(divToChange, shot) {
 
+  function playSound(soundFile) {
+
+    const audio = document.createElement('audio');
+    audio.setAttribute('src', soundFile);
+    audio.play();
+
+  }
+
   // pintando hit o water en div correspondiente
+
+  divToChange.style.backgroundSize = 'cover';
+  divToChange.style.transition = '0.75s ease';
+  divToChange.style.transform = 'scale(1.5)';
 
   if (shot === hit) {
 
     // pintando tocado
 
-    divToChange.innerHTML = '<img src="images/fire.jpg" width=50px height=50px>';
+    playSound('sounds/boom.mp3');
+    divToChange.style.backgroundImage = 'url("images/fire.jpg")';
+    divToChange.style.transform = 'scale(.9)';
+    // divToChange.innerHTML = '<img src="images/fire.jpg" width=50px height=50px>';
 
   } else {
 
     // pintando agua
 
-    divToChange.innerHTML = '<img src="images/water.jpg" width=50px height=50px>';
+    // playSound('sounds/water.mp3');
+    divToChange.style.backgroundImage = 'url("images/water.jpg")';
+    divToChange.style.transform = 'scale(.9)';
+    // divToChange.innerHTML = '<img src="images/water.jpg" width=50px height=50px>';
 
   }
-  // divToChange.textContent = shot;
 
 }
 
@@ -378,8 +520,8 @@ function clickedCell() {
 function boatsLeft(checkedBoard) {
 
   let boatsLeft = false;
-  const initialCheckedBoard = checkedBoard === 'Oponent' ? initialOponentBoard : initialComputerBoard;
-  const gameCheckedBoard = checkedBoard === 'Oponent' ? gameOponentBoard : gameComputerBoard;
+  const initialCheckedBoard = checkedBoard === oponentName ? initialOponentBoard : initialComputerBoard;
+  const gameCheckedBoard = checkedBoard === oponentName ? gameOponentBoard : gameComputerBoard;
 
   for (let i = 0; i < maxRows; i++) {
 
@@ -403,7 +545,7 @@ function boatsLeft(checkedBoard) {
 
 function playComputer() {
 
-  console.log('turno de ordenador');
+  console.log('Turno de ordenador');
 
   do {
     row = Math.floor(Math.random() * 10);
@@ -434,12 +576,13 @@ function playComputer() {
 
   }
 
-  checkedBoard = 'Oponent';
+  checkedBoard = oponentName;
 
   if (boatsLeft(checkedBoard)) {
 
     isComputerTurn = !isComputerTurn;
-    turnInfo.textContent = `Turno de: ${isComputerTurn ? 'Ordenador' : 'Oponente'}`;
+    turnInfo.textContent = `Turno de: ${isComputerTurn ? 'Ordenador' : oponentName}`;
+    console.log('Turno de ' + oponentName);
 
   } else {
 
@@ -451,7 +594,7 @@ function playComputer() {
 
 function playOponent(divToChange) {
 
-  console.log('turno de oponente');
+  // console.log('Turno de ' + oponentName);
 
   const divToChangeId = divToChange.id;
 
@@ -496,7 +639,11 @@ function playOponent(divToChange) {
       if (shot !== hit) {
 
         isComputerTurn = !isComputerTurn;
-        turnInfo.textContent = `Turno de: ${isComputerTurn ? 'Ordenador' : 'Oponente'}`;
+        turnInfo.textContent = `Turno de: ${isComputerTurn ? 'Ordenador' : oponentName}`;
+
+        // for (let i = 0; i < 5000000; i++) {
+        // }
+
         playComputer();
 
       } else {
@@ -520,7 +667,11 @@ function resetBoards() {
   for (let i = 0; i < 100; i++) {
 
     document.getElementById(`board1Id${i}`).textContent = '';
+    document.getElementById(`board1Id${i}`).style.backgroundImage = '';
+    document.getElementById(`board1Id${i}`).style.transform = 'scale(1)';
     document.getElementById(`board2Id${i}`).textContent = '';
+    document.getElementById(`board2Id${i}`).style.backgroundImage = '';
+    document.getElementById(`board2Id${i}`).style.transform = 'scale(1)';
     numberOfShots = 0;
     shotsInfo.textContent = `Número de jugadas: ${numberOfShots}`;
 
@@ -582,7 +733,7 @@ function endGame() {
 
   playing = false;
   turnInfo.setAttribute('class', 'endGame');
-  const endGameInfo = (checkedBoard === 'Oponent') ? 'Ordenador' : 'Oponente';
-  turnInfo.innerHTML = 'Juego terminado - Gana el ' + endGameInfo;
+  const endGameInfo = (checkedBoard === oponentName) ? 'ordenador' : oponentName;
+  turnInfo.innerHTML = 'Juego terminado - Gana ' + endGameInfo;
 
 }
